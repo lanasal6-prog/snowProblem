cb6eea42447bb6dacd8381f9de83a8c0bd81a12d
lanasal6-prog <lanaalsalamah6@gmail.com>
2026-05-04 18:22:03 +0100

    test

---
+++ SnowProb.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * This java code is the snow problem
+ * A frame by the name of snowProblemFrame is created for the GUI window.
+ * @param xxxxx
+ * @author Lana Alsalamah
+ * @return
+ **/
+
+public class SnowProb {
+    
+}
